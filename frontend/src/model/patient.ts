204500c3cd9acb8f4e681227f85e19722b3fa2dd
vunt199802-coder <vunt199802@gmail.com>
2023-08-13 13:05:35 +0200

    Update models.

--- frontend/src/model/patient.ts
+++ frontend/src/model/patient.ts
@@ -1,12 +1,12 @@
 export class Patient {
-    _id: string;
-    firstname: string;
-    lastname: string;
-    username: string;
-    password: string;
-    approved: boolean;
-    address: string;
-    phone: string;
-    mail: string;
-    image: string;
+    _id: string = ""
+    firstname: string = ""
+    lastname: string = ""
+    username: string = ""
+    password: string = ""
+    approved: boolean = false
+    address: string = ""
+    phone: string = ""
+    mail: string = ""
+    image: string = ""
 }
--- frontend/src/model/promotion.ts
+++
@@ -1,7 +0,0 @@
-export class Promotion {
-    _id: string;
-    name: string;
-    description: string;
-    price: number;
-    tillDate: Date;
-}
