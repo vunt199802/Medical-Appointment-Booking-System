d00b2de4522258f0e57ac24bf4da5a6a8f2610f7
vunt199802-coder <vunt199802@gmail.com>
2023-08-16 10:35:57 +0200

    Update sort.

--- frontend/src/app/doctors-cards/doctors-cards.component.ts
+++ frontend/src/app/doctors-cards/doctors-cards.component.ts
@@ -3,40 +3,101 @@ import {Doctor} from "../../model/doctor";
 import {DoctorService} from "../services/doctor.service";
 
 @Component({
-    selector: 'app-doctors-cards',
-    templateUrl: './doctors-cards.component.html',
-    styleUrls: ['./doctors-cards.component.css']
+  selector: 'app-doctors-cards',
+  templateUrl: './doctors-cards.component.html',
+  styleUrls: ['./doctors-cards.component.css']
 })
 export class DoctorsCardsComponent implements OnInit {
 
-    constructor(private serviceDoctor: DoctorService) {
-        this.getAllDoctors()
-    }
+  constructor(private serviceDoctor: DoctorService) {
+    this.getAllDoctors()
+  }
 
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {
+  }
 
-    doctors: Doctor[]
-    searchedFirstname = "";
-    searchedLastname = "";
-    searchedSpecialization = "";
-    searchResult: Doctor[];
+  doctors: Doctor[]
+  searchedFirstname = "";
+  searchedLastname = "";
+  searchedSpecialization = "";
+  searchResult: Doctor[];
 
-    getAllDoctors() {
-        this.serviceDoctor.readAll().subscribe((doctors: Doctor[]) => {
-            this.doctors = doctors
-        })
-    }
+  getAllDoctors() {
+    this.serviceDoctor.readAll().subscribe((doctors: Doctor[]) => {
+      this.doctors = doctors
+    })
+  }
 
-    search() {
-        let data = {
-            firstname: this.searchedFirstname,
-            lastname: this.searchedLastname,
-            specialization: this.searchedSpecialization
-        }
-        this.serviceDoctor.search({data}).subscribe((doctors: Doctor[]) => {
-            this.searchResult = doctors
-            console.log(this.searchResult)
-        });
+  buttonSortPressed = {
+    name: false,
+    lastname: false,
+    specialization: false
+  }
+
+  buttonSort(buttonId: string) {
+    let button = document.getElementById(buttonId)
+    this.buttonSortPressed.name = !this.buttonSortPressed.name
+    if (this.buttonSortPressed.name)
+      button.classList.add("active")
+    else
+      button.classList.remove("active")
+
+    switch (buttonId) {
+      case "buttonSortName":
+        if (this.buttonSortPressed.name)
+          this.sortByName()
+        break
+      case "buttonSortLastname":
+        if (this.buttonSortPressed.lastname)
+          this.sortByLastname()
+        break
+      case "buttonSortSpecialization":
+        if (this.buttonSortPressed.specialization)
+          this.sortBySpecialization()
+        break
     }
+  }
+
+  search() {
+    let data = {
+      firstname: this.searchedFirstname,
+      lastname: this.searchedLastname,
+      specialization: this.searchedSpecialization
+    }
+    this.serviceDoctor.search({data}).subscribe((doctors: Doctor[]) => {
+      this.searchResult = doctors
+      console.log(this.searchResult)
+    });
+  }
+
+  sortByName() {
+    this.doctors.sort((a, b) => {
+      if (a.firstname > b.firstname)
+        return 1;
+      if (a.firstname < b.firstname)
+        return -1;
+      return 0;
+    })
+  }
+
+  sortByLastname() {
+    this.doctors.sort((a, b) => {
+      if (a.lastname > b.lastname)
+        return 1;
+      if (a.lastname < b.lastname)
+        return -1;
+      return 0;
+    })
+  }
+
+  sortBySpecialization() {
+    this.doctors.sort((a, b) => {
+      if (a.specialization > b.specialization)
+        return 1;
+      if (a.specialization < b.specialization)
+        return -1;
+      return 0;
+    })
+  }
+
 }
