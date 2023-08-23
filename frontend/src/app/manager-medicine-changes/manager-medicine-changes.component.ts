b15b80ee6c10515baa5381f961fcd8f17e7982cf
vunt199802-coder <vunt199802@gmail.com>
2023-08-23 11:37:32 +0200

    Clear component.

--- frontend/src/app/manager-medicine-changes/manager-medicine-changes.component.ts
+++ frontend/src/app/manager-medicine-changes/manager-medicine-changes.component.ts
@@ -5,36 +5,36 @@ import {AppointmentType} from "../../model/appointmentType";
 import {AppointmentTypeService} from "../services/appointmentType.service";
 
 @Component({
-    selector: 'app-manager-medicine-changes',
-    templateUrl: './manager-medicine-changes.component.html',
-    styleUrls: ['./manager-medicine-changes.component.css']
+  selector: 'app-manager-medicine-changes',
+  templateUrl: './manager-medicine-changes.component.html',
+  styleUrls: ['./manager-medicine-changes.component.css']
 })
 export class ManagerMedicineChangesComponent implements OnInit {
 
-    constructor(private serviceAppointmentType: AppointmentTypeService, private serviceSpecialization: SpecializationService) {
-        this.getAllAppointmentTypes()
-        this.getAllSpecializations()
-    }
-
-  saveChanges() {
-
+  constructor(private serviceAppointmentType: AppointmentTypeService, private serviceSpecialization: SpecializationService) {
+    this.getAllAppointmentTypes()
+    this.getAllSpecializations()
   }
 
-    ngOnInit(): void {
-    }
+  saveChanges(appointment) {
+    console.log(appointment)
+  }
 
-    appointmentTypes: AppointmentType[]
-    specializations: Specialization[]
+  ngOnInit(): void {
+  }
 
-    getAllAppointmentTypes() {
-        this.serviceAppointmentType.readAll().subscribe((appointmentTypes: AppointmentType[]) => {
-            this.appointmentTypes = appointmentTypes
-        })
-    }
+  appointmentTypes: AppointmentType[]
+  specializations: Specialization[]
 
-    getAllSpecializations() {
-        this.serviceSpecialization.readAll().subscribe((specializations: Specialization[]) => {
-            this.specializations = specializations
-        })
-    }
+  getAllAppointmentTypes() {
+    this.serviceAppointmentType.readAll().subscribe((appointmentTypes: AppointmentType[]) => {
+      this.appointmentTypes = appointmentTypes
+    })
+  }
+
+  getAllSpecializations() {
+    this.serviceSpecialization.readAll().subscribe((specializations: Specialization[]) => {
+      this.specializations = specializations
+    })
+  }
 }
