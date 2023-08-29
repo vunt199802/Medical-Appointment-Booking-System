8c3c20dec35b6ffff2eacec788823e2fce1fb951
vunt199802-coder <vunt199802@gmail.com>
2023-08-29 10:06:18 +0200

    Update navigation-doctor.component.ts

--- frontend/src/app/navigation-doctor/navigation-doctor.component.ts
+++ frontend/src/app/navigation-doctor/navigation-doctor.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
 import {DoctorService} from "../services/doctor.service";
 import {Router} from "@angular/router";
 
